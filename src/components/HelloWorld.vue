c960075638ecc911df57c8f0395c955e74642234
happyt <ian.happytuesdays@gmail.com>
2019-07-27 09:21:52 +0100

    Just a quick test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,248 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-flex xs12>
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="50"></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">Hamoni test</h1>
+        <p class="subheading font-weight-regular">General dicussion about tables edit</p>
+      </v-flex>
+
+      <v-flex mb-5 xs12>
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >{{ next.text }}</a>
+        </v-layout>
+      </v-flex>
+      <v-flex mb-5 xs12>
+        <div>
+          <v-dialog v-model="dialog" max-width="500px">
+            <v-btn slot="activator" color="primary" dark class="mb-2">New Item</v-btn>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container grid-list-md>
+                  <v-layout wrap>
+                    <v-flex xs12 sm6 md4>
+                      <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm6 md4>
+                      <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm6 md4>
+                      <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm6 md4>
+                      <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm6 md4>
+                      <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+                <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-data-table
+            :headers="headers"
+            :items="desserts"
+            hide-default-footer
+            class="elevation-1"
+          >
+            <template slot="items" slot-scope="props">
+              <td>{{ props.item.name }}</td>
+              <td class="text-xs-right">{{ props.item.calories }}</td>
+              <td class="text-xs-right">{{ props.item.fat }}</td>
+              <td class="text-xs-right">{{ props.item.carbs }}</td>
+              <td class="text-xs-right">{{ props.item.protein }}</td>
+              <td class="justify-center layout px-0">
+                <v-btn icon class="mx-0" @click="editItem(props.item)">
+                  <v-icon color="teal">edit</v-icon>
+                </v-btn>
+                <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+                  <v-icon color="pink">delete</v-icon>
+                </v-btn>
+              </td>
+            </template>
+          </v-data-table>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data: () => ({
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer"
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/layout/pre-defined"
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
+      }
+    ],
+    dialog: false,
+    headers: [
+      {
+        text: "Dessert (100g serving)",
+        align: "left",
+        sortable: false,
+        value: "name"
+      },
+      { text: "Calories", value: "calories" },
+      { text: "Fat (g)", value: "fat" },
+      { text: "Carbs (g)", value: "carbs" },
+      { text: "Protein (g)", value: "protein" },
+      { text: "Actions", value: "name", sortable: false }
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: { name: "", calories: 0, fat: 0, carbs: 0, protein: 0 },
+    defaultItem: { name: "", calories: 0, fat: 0, carbs: 0, protein: 0 },
+    listPrimitive: null
+  }),
+  methods: {
+    initialise(hamoni) {
+      hamoni
+        .createList("vue-table", [
+          {
+            name: "Frozen Yogurt",
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0
+          },
+          {
+            name: "Ice cream sandwich",
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3
+          },
+          {
+            name: "Eclair",
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0
+          }
+        ])
+        .then(primitive => {
+          this.listPrimitive = primitive;
+          this.desserts = this.listPrimitive.getAll();
+        })
+        .catch(alert);
+    },
+    subscribeToUpdate() {
+      this.listPrimitive.onItemAdded(item => {
+        this.desserts.push(item.value);
+      });
+      this.listPrimitive.onItemUpdated(item => {
+        //update the item at item.index
+        this.desserts.splice(item.index, 1, item.value);
+      });
+      this.listPrimitive.onItemRemoved(item => {
+        //remove the item at item.index
+        this.desserts.splice(item.index, 1);
+      });
+    },
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+    deleteItem(item) {
+      const index = this.desserts.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.listPrimitive.delete(index);
+    },
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        this.listPrimitive.update(this.editedIndex, this.editedItem);
+      } else {
+        this.listPrimitive.push(this.editedItem);
+      }
+      this.close();
+    }
+  },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    }
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+  mounted: async function() {
+    const accountId = env.id;
+    const appId = env.appId;
+    const response = await fetch("https://api.sync.hamoni.tech/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify({ accountId, appId })
+    });
+
+    console.log("pretoken: ");
+    const token = await response.json();
+
+    console.log("token: ", token);
+
+    let hamoni = new Hamoni(token);
+    hamoni
+      .connect()
+      .then(() => {
+        console.log("connected");
+        hamoni
+          .get("vue-table")
+          .then(primitive => {
+            this.listPrimitive = primitive;
+            this.desserts = [...primitive.getAll()];
+            this.subscribeToUpdate();
+          })
+          .catch(error => {
+            console.log("err");
+            if (error === "Error getting state from server") {
+              this.initialise(hamoni);
+            } else {
+              alert(error);
+            }
+          });
+      })
+      .catch(alert)
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
